0d890a9f7c6afd1c008fcba4481d4975f72a52e8
HlebBekesevich <bigdogkrok@gmail.com>
2019-04-17 10:43:55 +0300

    fixed js

--- hw_js2/app/js/script.js
+++ hw_js2/app/js/script.js
@@ -3,20 +3,18 @@
     let arr = [];
 	function simple(first, last) 
 	{
-		if(last>3) 
-		{
-			arr.push(3);
-			i=3;
-		}
-		else i=first;
-		if(last>5) 
+		if(last<2&&first<2) return 'ERROR';
+		if(last>=2&&first<=2) arr.push(2);
+		if(last>=3&&first<=2) arr.push(3);
+		if(last>=5&&first<=5) 
 		{
 			arr.push(5);
 			i=5;
 		}
+		else if(first>5) i=first;
+		else return arr;
 		for(;i<=last;i++)
 		{
-			if(i<2) i=2;
 			if((i%2)&&(i%3)&&(i%5))
 				arr.push(i);
 		}
