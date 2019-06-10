33eecc9a3008e283eee5bab0fcafa442d94d5c42
HlebBekesevich <bigdogkrok@gmail.com>
2019-06-10 15:29:50 +0300

    exam

--- exam/app/js/script.js
+++ exam/app/js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
 			"transform": "rotate(90deg)",
 			"margin": "30px 0 90px 0"
 		}).show("slow").css({
-			"display": "block",
 			"transform": "rotate(0)",
 			"margin": "60px 0 60px -33px"
 		});
@@ -23,7 +22,6 @@ $(document).ready(function(){
 			"transform": "rotate(90deg)",
 			"margin": "30px 0 90px 0"
 		}).show("slow").css({
-			"display": "block",
 			"transform": "rotate(0)",
 			"margin": "60px 0 60px -33px"
 		});
@@ -31,7 +29,14 @@ $(document).ready(function(){
 		$(".l1").removeClass("active");
 	}, function(){
 		$(".l1").addClass("active");
-		$(".n1").css("display", "block");
+		$(".n1").css({
+			"display": "block",
+			"transform": "rotate(0)",
+			"margin": "60px 0 60px -33px"
+		}).show("slow").css({
+			"transform": "rotate(90deg)",
+			"margin": "30px 0 90px 0"
+		});
 		$(".n2").css("display", "none");
 	});
 
@@ -41,7 +46,6 @@ $(document).ready(function(){
 			"transform": "rotate(90deg)",
 			"margin": "30px 0 90px 0"
 		}).show("slow").css({
-			"display": "block",
 			"transform": "rotate(0)",
 			"margin": "60px 0 60px -33px"
 		});
@@ -49,7 +53,14 @@ $(document).ready(function(){
 		$(".l1").removeClass("active");
 	}, function(){
 		$(".l1").addClass("active");
-		$(".n1").css("display", "block");
+		$(".n1").css({
+			"display": "block",
+			"transform": "rotate(0)",
+			"margin": "60px 0 60px -33px"
+		}).show("slow").css({
+			"transform": "rotate(90deg)",
+			"margin": "30px 0 90px 0"
+		});
 		$(".n3").css("display", "none");
 	});
 
@@ -59,7 +70,6 @@ $(document).ready(function(){
 			"transform": "rotate(90deg)",
 			"margin": "30px 0 90px 0"
 		}).show("slow").css({
-			"display": "block",
 			"transform": "rotate(0)",
 			"margin": "60px 0 60px -33px"
 		});
@@ -67,7 +77,14 @@ $(document).ready(function(){
 		$(".l1").removeClass("active");
 	}, function(){
 		$(".l1").addClass("active");
-		$(".n1").css("display", "block");
+		$(".n1").css({
+			"display": "block",
+			"transform": "rotate(0)",
+			"margin": "60px 0 60px -33px"
+		}).show("slow").css({
+			"transform": "rotate(90deg)",
+			"margin": "30px 0 90px 0"
+		});
 		$(".n4").css("display", "none");
 	});
 
@@ -77,7 +94,6 @@ $(document).ready(function(){
 			"transform": "rotate(90deg)",
 			"margin": "30px 0 90px 0"
 		}).show("slow").css({
-			"display": "block",
 			"transform": "rotate(0)",
 			"margin": "60px 0 60px -33px"
 		});
@@ -85,7 +101,14 @@ $(document).ready(function(){
 		$(".l1").removeClass("active");
 	}, function(){
 		$(".l1").addClass("active");
-		$(".n1").css("display", "block");
+		$(".n1").css({
+			"display": "block",
+			"transform": "rotate(0)",
+			"margin": "60px 0 60px -33px"
+		}).show("slow").css({
+			"transform": "rotate(90deg)",
+			"margin": "30px 0 90px 0"
+		});
 		$(".n5").css("display", "none");
 	});
 
@@ -110,121 +133,121 @@ function initMap()
 		scrollwheel: false,
 		center: {lat: 40.7060129, lng: -73.942059},
 		styles: [
-	{
-		"featureType": "all",
-		"elementType": "geometry",
-		"stylers": [
 		{
-			"color": "#bea67c"
-		}
-		]
-	},
-	{
-		"featureType": "all",
-		"elementType": "labels.text.fill",
-		"stylers": [
-		{
-			"gamma": 0.01
+			"featureType": "all",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"color": "#bea67c"
+			}
+			]
 		},
 		{
-			"lightness": 20
-		}
-		]
-	},
-	{
-		"featureType": "all",
-		"elementType": "labels.text.stroke",
-		"stylers": [
-		{
-			"saturation": -31
+			"featureType": "all",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"gamma": 0.01
+			},
+			{
+				"lightness": 20
+			}
+			]
 		},
 		{
-			"lightness": -33
+			"featureType": "all",
+			"elementType": "labels.text.stroke",
+			"stylers": [
+			{
+				"saturation": -31
+			},
+			{
+				"lightness": -33
+			},
+			{
+				"weight": 2
+			},
+			{
+				"gamma": 0.8
+			}
+			]
 		},
 		{
-			"weight": 2
+			"featureType": "all",
+			"elementType": "labels.icon",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
 		},
 		{
-			"gamma": 0.8
-		}
-		]
-	},
-	{
-		"featureType": "all",
-		"elementType": "labels.icon",
-		"stylers": [
-		{
-			"visibility": "off"
-		}
-		]
-	},
-	{
-		"featureType": "landscape",
-		"elementType": "geometry",
-		"stylers": [
-		{
-			"lightness": 30
+			"featureType": "landscape",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"lightness": 30
+			},
+			{
+				"saturation": 30
+			}
+			]
 		},
 		{
-			"saturation": 30
-		}
-		]
-	},
-	{
-		"featureType": "poi",
-		"elementType": "geometry",
-		"stylers": [
-		{
-			"saturation": 20
-		}
-		]
-	},
-	{
-		"featureType": "poi.park",
-		"elementType": "geometry",
-		"stylers": [
-		{
-			"lightness": 20
+			"featureType": "poi",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"saturation": 20
+			}
+			]
 		},
 		{
-			"saturation": -20
-		}
-		]
-	},
-	{
-		"featureType": "road",
-		"elementType": "geometry",
-		"stylers": [
-		{
-			"lightness": 10
+			"featureType": "poi.park",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"lightness": 20
+			},
+			{
+				"saturation": -20
+			}
+			]
 		},
 		{
-			"saturation": -30
-		}
-		]
-	},
-	{
-		"featureType": "road",
-		"elementType": "geometry.stroke",
-		"stylers": [
-		{
-			"saturation": 25
+			"featureType": "road",
+			"elementType": "geometry",
+			"stylers": [
+			{
+				"lightness": 10
+			},
+			{
+				"saturation": -30
+			}
+			]
 		},
 		{
-			"lightness": 25
-		}
-		]
-	},
-	{
-		"featureType": "water",
-		"elementType": "all",
-		"stylers": [
+			"featureType": "road",
+			"elementType": "geometry.stroke",
+			"stylers": [
+			{
+				"saturation": 25
+			},
+			{
+				"lightness": 25
+			}
+			]
+		},
 		{
-			"lightness": -20
+			"featureType": "water",
+			"elementType": "all",
+			"stylers": [
+			{
+				"lightness": -20
+			}
+			]
 		}
 		]
-	}
-	]
 	};
 
 	var map = new google.maps.Map(element, options);
@@ -243,12 +266,12 @@ $(".contacts__map_icon").click(function(){
 
 
 window.onscroll = () => {
-    if (window.pageYOffset >= document.documentElement.scrollHeight-document.documentElement.clientHeight-80){
-    	$(".contacts__map_icon").addClass("active");
-    	console.log("add");
-    } else {
-    	$(".contacts__map_icon").removeClass("active");
-  }
+	if (window.pageYOffset >= document.documentElement.scrollHeight-document.documentElement.clientHeight-80){
+		$(".contacts__map_icon").addClass("active");
+		console.log("add");
+	} else {
+		$(".contacts__map_icon").removeClass("active");
+	}
 }
 
 
